669681bdae0e9ea2756f114d4b6dfbb6e114ed15
regonn <programmer@haizine.art>
2020-06-13 10:27:15 +0900

    dotenv を利用

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
-// import { config } from 'dotenv'
-// import replace from '@rollup/plugin-replace'
+import dotenv from 'rollup-plugin-dotenv'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -17,14 +16,7 @@ export default {
         file: 'public/build/bundle.js',
     },
     plugins: [
-        // replace({
-        //     process: JSON.stringify({
-        //         env: {
-        //             isProd: production,
-        //             // ...config().parsed,
-        //         },
-        //     }),
-        // }),
+        dotenv(),
         svelte({
             // enable run-time checks when not in production
             dev: !production,
